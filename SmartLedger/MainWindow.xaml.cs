2277311de27c7577d5ad7bb2147cd3b7a55c16c9
AhmetAktas67 <ahmet67aktas@gmail.com>
2026-08-14 20:21:19 +0200

    Erstes DataGrid-Binding funktioniert: MainWindow zeigt Mitgliederliste

--- SmartLedger/MainWindow.xaml.cs
+++ SmartLedger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using SmartLedger.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SmartLedger.ViewModels;
 
 namespace SmartLedger
 {
@@ -19,6 +21,7 @@ namespace SmartLedger
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
