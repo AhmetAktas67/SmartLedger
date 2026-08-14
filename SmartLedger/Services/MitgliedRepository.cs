c62d7eaab3be4459ef0aa335665db28d9286bd79
AhmetAktas67 <ahmet67aktas@gmail.com>
2026-08-14 20:39:08 +0200

    MitgliedRepository für DB-Zugriff, MainViewModel lädt Mitglieder aus SQLite

---
+++ SmartLedger/Services/MitgliedRepository.cs
@@ -0,0 +1,27 @@
+﻿using SmartLedger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartLedger.Models;
+
+namespace SmartLedger.Services
+{
+    internal class MitgliedRepository
+    {
+        public List<Mitglied> GetAlle()
+        {
+            using var db = new SmartLedgerDbContext();
+            return db.Mitglieder.ToList();
+        }
+
+        public void Speichern(Mitglied mitglied)
+        {
+            using var db = new SmartLedgerDbContext();
+            db.Mitglieder.Add(mitglied);
+            db.SaveChanges();
+        }
+    }
+}
+
